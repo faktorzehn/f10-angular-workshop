84b162e3f7606d6b475d993533244381570ab3e0
Thomas Wruß <thomas.wruss@faktorzehn.at>
2023-06-09 10:50:02 +0200

    Kapitel 5

---
+++ src/app/core/tasks.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Task } from '../tasks/shared/task.model';
+import { Observable } from 'rxjs';
+
+/**
+ * Ein Service wird als Singelton verwendet. In unserem Beispiel kommunizieren wir hier mit dem Backend.
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class TasksService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Task[]> {
+    return this.http.get<Task[]>(`${environment.BACKEND}/tasks`);
+  }
+}
--- src/app/tasks/task-list-page/task-list-page.component.ts
+++ src/app/tasks/task-list-page/task-list-page.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { Task } from '../shared/task.model';
+import { TasksService } from 'src/app/core/tasks.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
+
+/**
+ * Die Komponente ist der Einstiegspunkt für die Listen Darstellung von Tasks.
+ * Es werden die Tasks vom Service geholt und dann sortiert. Bei einem Klick auf ein Task oder dem "add button" wird die Navigation ausgelöst.
+ */
 @Component({
   selector: 'app-task-list-page',
   templateUrl: './task-list-page.component.html',
@@ -8,8 +15,37 @@ import { Task } from '../shared/task.model';
 })
 export class TaskListPageComponent {
 
-  tasks?: Task[] = [
-    {id: "1", title: "Schiff entern", description: "Das Schiff von der englischen Flotte entern.", priority: 5, destination: "Bermuda Dreieck", type:"entern", gold: 1000}
-  ]
+  tasks?: Task[];
+  sortByCoin?: 'UP' | 'DOWN';
 
+  constructor(tasksService: TasksService) {
+    tasksService.getAll().pipe(takeUntilDestroyed()).subscribe(t => {
+      this.tasks = t;
+      this.sortTasks(this.sortByCoin);
+    });
+  }
+
+  onSortByCoin() {
+    // DOWN - UP - UNDEFINED
+    if(this.sortByCoin === 'DOWN') {
+      this.sortByCoin = 'UP';
+    } else if(this.sortByCoin === 'UP') {
+      this.sortByCoin = undefined;
+    } else {
+      this.sortByCoin = 'DOWN';
+    }
+
+    this.sortTasks(this.sortByCoin);
+    return this.sortByCoin;
+  }
+
+  private sortTasks(sort?: 'UP' | 'DOWN') {
+    if(sort === 'DOWN') {
+      this.tasks?.sort((t1, t2)=>t2.gold-t1.gold);
+    }else if(this.sortByCoin === 'UP') {
+      this.tasks?.sort((t1, t2)=>t1.gold-t2.gold);
+    } else {
+      this.tasks?.sort((t1, t2)=>t2.priority-t1.priority);
+    }
+  }
 }
